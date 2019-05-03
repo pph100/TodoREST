310e56281c726d2a6e399d71cadd6a735d939311
Uli Petersen <dev@upworx.net>
2019-05-03 16:29:03 +0200

    enable non-encrypted HTTP session access for iOS

---
+++ TodoREST/Views/TabPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TodoREST
+{
+    public partial class TodoListPage : ContentPage
+    {
+        // bool alertShown = false;
+
+        public TodoListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listView.ItemsSource = await App.TodoManager.GetTasksAsync();
+        }
+
+        void OnAddItemClicked(object sender, EventArgs e)
+        {
+            var todoItem = new TodoItem()
+            {
+                ID = Guid.NewGuid().ToString(),
+                DttmCreated = System.DateTime.Now.ToString()
+            };
+            var todoPage = new TodoItemPage(true);
+            todoPage.BindingContext = todoItem;
+            Navigation.PushAsync(todoPage);
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var todoItem = e.SelectedItem as TodoItem;
+            var todoPage = new TodoItemPage();
+
+            todoPage.BindingContext = todoItem;
+            Navigation.PushAsync(todoPage);
+        }
+    }
+}
